93a67214991d6eb1ece2db969354b0217b6cd8c6
namgivu <namgivu@gmail.com>
2020-09-05 04:51:12 +0700

    clarify w comment @ healthcheck

--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -4,16 +4,19 @@ AH=`cd "$SH/.." && pwd`  # AH aka APP_HOME
 
     source "$AH/docker/.config.sh"
 
+        # ensure container is up by grepping the its log
         l=`docker logs $CONTAINER_NAME 2>&1`; echo $l; echo $l | grep "api server started at port=3000"
             api_is_serving=$?
 
+        # ensure port matched with :PORT set in .config.sh
         g=`docker ps --format '{{.Names}} {{.Ports}}' | grep $CONTAINER_NAME` ; echo $g | grep -o ":$PORT->3000"
-            api_port_matched_w_config=$?
+            correct_port=$?
+
 
 echo "
 --- ---
 SUMMARY
-                 check item = error status
-             api_is_serving = $api_is_serving
-  api_port_matched_w_config = $api_port_matched_w_config
+      check item = error status
+  api_is_serving = $api_is_serving
+    correct_port = $correct_port
 "
