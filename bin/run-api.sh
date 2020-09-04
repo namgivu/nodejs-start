d82cf0b12c6f64bdaaa495f048b6e7d621d4cb3c
namgivu <namgivu@gmail.com>
2020-09-05 04:27:01 +0700

    aftermath only when succeeded

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 SH=`cd $(dirname $BASH_SOURCE) && pwd`  # SH aka SCRIPT_HOME=executed script's path, containing folder
-# shellcheck disable=SC2006
 AH=`cd "$SH/.." && pwd`  # AH aka APP_HOME
 
 if [[ -z $PORT ]]; then PORT=3000; fi
--- docker/run.sh
+++ docker/run.sh
@@ -10,9 +10,11 @@ AH=`cd "$SH/.." && pwd`  # AH aka APP_HOME
         # load custom mapped exposed-port if nay - default 3000 if not specified
         if [[ -z $PORT ]]; then PORT=3000; fi
 
-        # run api
-        docker run  --name $CONTAINER_NAME  -d              -p 3000:$PORT  $IMAGE_TAG
-        #            .                      run as daemon   .              .
+        set -e  # halt if error ON
+            # run api
+            docker run  --name $CONTAINER_NAME  -d              -p $PORT:3000  $IMAGE_TAG
+            #            .                      run as daemon   .              .
+        set +e  # halt if error OFF
 
             # aftermath check
             echo
