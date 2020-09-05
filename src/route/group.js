65a822c73d48fd77f525a2c6d02ed6c43403c628
namgivu <namgivu@gmail.com>
2020-09-05 21:29:42 +0700

    open postgres connection 0th via pg pool

--- src/route/group.js
+++ src/route/group.js
@@ -1,3 +1,12 @@
+const pg = require('pg')
+
+const pool = new pg.Pool({
+    'user'     : 'nodejs_start_dbuser20909',
+    'password' : 'nodejs_start_dbpass20909',
+    'port'     : 20908,
+    'database' : 'nodejs_start_dbname20909',
+})
+
 exports.on_get = (req, resp) => {
     resp.status(200).json({})
 }
