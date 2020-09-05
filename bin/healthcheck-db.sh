71571d088936e4e6e6dbf5324af408321979d04c
namgivu <namgivu@gmail.com>
2020-09-05 19:34:45 +0700

    direct_connectivity @ healthcheck db

--- bin/healthcheck-db.sh
+++ bin/healthcheck-db.sh
@@ -10,11 +10,16 @@ AH=`cd "$SH/.." && pwd`  # AH aka APP_HOME
                 ec=$? && echo $ec; db_is_serving=$ec
 
         echo '--- ---'
-        echo 'ensure postgres db working'
-            docker exec $CONTAINER_NAME  psql postgresql://$DB_USER:$DB_PASS@:/$DB_NAME -c '\dt' -tA  #NOTE no DB_PORT here cause within the container, port is still 5432
+        echo 'ensure postgres connectivity'
+            docker exec $CONTAINER_NAME  psql "postgresql://$DB_USER:$DB_PASS@:/$DB_NAME" -c '\dt'  #NOTE no DB_PORT here cause within the container, port is still 5432
+                ec=$? && echo $ec; container_connectivity=$ec
+            pg_isready -U $DB_USER -h localhost -p $DB_PORT -d $DB_NAME  # ref. https://stackoverflow.com/a/44496546/248616
+                ec=$? && echo $ec; direct_connectivity=$ec
 echo "
 --- ---
 SUMMARY
-      check item = error status
-   db_is_serving = $db_is_serving
+               check item = error status
+            db_is_serving = $db_is_serving
+   container_connectivity = $container_connectivity
+      direct_connectivity = $direct_connectivity
 "
